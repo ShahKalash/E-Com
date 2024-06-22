2aa2849975ba7f44ffcb2b4c8547d9f6554aca21
Kalash <kalash.s@ahduni.edu.in>
2024-06-22 14:37:29 +0530

    Add footer

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,6 @@ import React from 'react'
 import styled from 'styled-components';
 import HeroSection from './components/HeroSection';
 import Services from './components/Services';
-
 const Home = () => {
     return (<>
         <HeroSection props={{ name: "Flip-Zone" }} />
