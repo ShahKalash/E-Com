34e3a3de774a1154b17859b2e11324a229b64228
Kalash <kalash.s@ahduni.edu.in>
2024-06-25 13:50:12 +0530

    Add Reducer and fetch data from API

---
+++ src/Reducer/ProductReducer.js
@@ -0,0 +1,30 @@
+const ProductReducer = (state, action) => {
+    if (action.type === "SET_LOADING") {
+        return {
+            ...state,
+            isLoading: true
+        };
+    }
+    if (action.type === "SET_API_DATA") {
+        const featureData = action.payload.filter((curElem) => {
+            return curElem.featured === true;
+        });
+
+        return {
+            ...state,
+            isLoading: false,
+            products: action.payload,
+            featureProducts: featureData
+        }
+    }
+    if (action.type === "API_ERROR") {
+        return {
+            ...state,
+            isLoading: false,
+            isError: true
+        };
+    }
+    return state;
+};
+
+export default ProductReducer
