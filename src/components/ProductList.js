68114c1a2f57c83b7b4bed7129e55d8f997d5515
Kalash <kalash.s@ahduni.edu.in>
2024-07-05 15:17:36 +0530

    Add Reducers

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useFilterContext } from '../context/filter_context';
+import GridView from './GridView';
 
 const ProductList = () => {
-    return (
-        <div>ProductList</div>
-    )
-}
+    const { filter_products, setGridView } = useFilterContext();
+
+    console.log(filter_products);
+    if (setGridView) {
+        return <GridView products={filter_products} />;
+    }
+    // if (setGridView === false){
+    //     return <ListView products={filter_products/>;
+    // }
+};
 
 export default ProductList;
--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -7,6 +7,7 @@ const FilterContext = createContext();
 const initialState = {
     filter_products: [],
     all_products: [],
+    grid_view: true,
 }
 
 export const FilterContextProvider = ({ children }) => {
@@ -15,11 +16,18 @@ export const FilterContextProvider = ({ children }) => {
 
     const [state, dispatch] = useReducer(reducer, initialState);
 
+
+    console.log(products);
+
     useEffect(() => {
-        dispatch({ type: "LOAD_FILTER_PRODUCTS", payload: products });
+        if (products)
+            dispatch({ type: "LOAD_FILTER_PRODUCTS", payload: products });
     }, [products]);
 
-    return (<FilterContext.Provider value={{ ...state }}>
+    const setGridView = () => {
+        return dispatch({ type: "SET_GRIDVIEW" });
+    }
+    return (<FilterContext.Provider value={{ ...state, setGridView }}>
         {children}
     </FilterContext.Provider>);
 };
